0ce57056f9ae738756d2ede7210f8e7b40b6069b
LGL001 <rjsqkddl1224@gmail.com>
2024-09-10 17:48:09 +0900

    Add files via upload

---
+++ 202410822 김건규.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("Input two untegers:");
+	int a1, a2;
+		scanf_s("%d %d", &a1, &a2);
+		printf("%d & %d = %d\n", a1, a2, a1 & a2);
+		printf("%d | %d = %d\n", a1, a2, a1 | a2);
+		printf("%d ^ %d = %d\n", a1, a2, a1 ^ a2);
+
+
+
+
+
+}
+		
+
+
+
+
+
+
+
+
